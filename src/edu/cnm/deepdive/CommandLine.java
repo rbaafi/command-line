493ee187ea5e3ed99ce9f0c153eb3b7c0edec54b
Richmond <rbaafi@cnm.edu>
2020-01-13 15:13:22 -0700

    Reading scanner for user/file input.

--- src/edu/cnm/deepdive/CommandLine.java
+++ src/edu/cnm/deepdive/CommandLine.java
@@ -1,11 +1,16 @@
 package edu.cnm.deepdive;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class CommandLine {
 
   public static void main(String[] args) {
-   int[] vals = parse(args);
-   int total = sum(vals);
-    System.out.println(total);
+    Double[] data = read();
+//   int total = sum(vals);
+//    System.out.println(total);
   }
 
   private static int[] parse(String[] args) {
@@ -19,8 +24,23 @@ public class CommandLine {
   private static int sum(int[] data) {
     int total = 0;
     for (int i = 0; i < data.length; i++) {
-      total += data
+      total += data[i];
     }
     return total;
+  }
+
+  private static Double[] read() {
+    Double[] data = null;
+    Scanner scanner = new Scanner(System.in);
+    List<Double> input = new LinkedList<>();
+    while (true) {
+      try {
+        input.add(scanner.nextDouble());
+      } catch (NoSuchElementException e) {
+        break;
+      }
+    }
+    data = input.toArray(new Double[0]);
+    return data;
   }
 }
